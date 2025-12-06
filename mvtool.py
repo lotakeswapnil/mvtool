c64e450e6fb67cd155d9cf345ed1837dbf3dcece
lotakeswapnil <lotakeswapnil@yahoo.com>
2025-12-06 21:59:24 +0530

    regression

--- mvtool.py
+++ mvtool.py
@@ -13,17 +13,17 @@ if uploaded is not None:
     df = pd.read_csv(uploaded)
     st.write('Preview:', df.head())
 
-target_col = st.text_input('Target column name (energy usage)')
+#target_col = st.text_input('Target column name (energy usage)')
 
-if target_col:
-    X = df.drop(columns=[target_col])
-    y = df[target_col]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+#if target_col:
+#    X = df.drop(columns=[target_col])
+#    y = df[target_col]
+#    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    model = LinearRegression()
-    model.fit(X_train, y_train)
-    preds = model.predict(X_test)
-    regression = model.score(X_test, y_test)
+#    model = LinearRegression()
+#    model.fit(X_train, y_train)
+#    preds = model.predict(X_test)
+#    regression = model.score(X_test, y_test)
 
-    st.write(f'Regression: {regression:.2%}')
-    st.line_chart(pd.DataFrame({'Actual': y_test, 'Predicted': preds}).reset_index(drop=True))
+#    st.write(f'Regression: {regression:.2%}')
+#    st.line_chart(pd.DataFrame({'Actual': y_test, 'Predicted': preds}).reset_index(drop=True))
