bd78918fed2c0b9906994f487be34f64d62b219e
lotakeswapnil <lotakeswapnil@yahoo.com>
2025-12-06 23:01:52 +0530

    regression

--- mvtool.py
+++ mvtool.py
@@ -16,11 +16,11 @@ if uploaded is not None:
 energy_cons = st.text_input('Target column name (energy usage)')
 num_var = st.number_input('Number of variables', min_value=1, max_value=10, step=1)
 for i in range(1,num_var+1):
-    globals()[f"ind_var{i}"] = st.text_input(
+    globals()[f"ind_var_{i}"] = st.text_input(
         f"Independent variable {i}",
         key=f"var_{i}"
     )
-   print(f"ind_var{i}")
+   st.write(globals()[f"ind_var_{i}"])
 
 if energy_cons and ind_var_1 is not None:
     X = df[ind_var_1].to_frame()
