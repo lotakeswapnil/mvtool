0758f76167d9b011ce9373166b70172eb2e23da6
lotakeswapnil <lotakeswapnil@yahoo.com>
2025-12-06 23:38:05 +0530

    regression

--- mvtool.py
+++ mvtool.py
@@ -34,8 +34,8 @@ if energy_cons is not None and globals()[f"ind_var_{i}"] != "" :
     st.write(f'Regression: {regression:.2%}')
     st.line_chart(pd.DataFrame({'Actual': y_test, 'Predicted': preds}).reset_index(drop=True))
 
-if globals()[f"ind_var_{i}"] not in df.columns.values:
-    st.write('Variable not found.')
+    else:
+        st.write('Variable not found.')
 
 else:
     st.write('All Variables not defined.')
