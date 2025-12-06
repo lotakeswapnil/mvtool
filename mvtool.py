fb8f0318d06cb6266c6da533f1f46dd06ca66e2a
lotakeswapnil <lotakeswapnil@yahoo.com>
2025-12-07 00:39:03 +0530

    regression

--- mvtool.py
+++ mvtool.py
@@ -27,7 +27,7 @@ if uploaded is not None:
 
     if energy_cons is not None and globals()[f"ind_var_{i}"] != "":
         if globals()[f"ind_var_{i}"] not in df.columns:
-            st.warning(f"Variable' not found in the uploaded CSV.")
+            st.error(f"Variable' not found in the uploaded CSV.")
         else:
             X = df[globals()[f'ind_var_{i}']].to_frame()
             y = df[energy_cons]
@@ -42,4 +42,4 @@ if uploaded is not None:
             st.line_chart(pd.DataFrame({'Actual': y_test, 'Predicted': preds}).reset_index(drop=True))
 
     else:
-        st.write('All Variables not defined.')
+        st.error('All Variables not defined.')
