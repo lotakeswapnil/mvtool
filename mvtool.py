a159f5f3322155b82178ba1b58b9335267e04af3
lotakeswapnil <lotakeswapnil@yahoo.com>
2025-12-06 23:20:18 +0530

    regression

--- mvtool.py
+++ mvtool.py
@@ -22,7 +22,7 @@ for i in range(1,num_var+1):
     )
 
 if energy_cons is not None and globals()[f"ind_var_{i}"] != "" :
-    X = df[f"ind_var_{i}"].to_frame()
+    X = df[ind_var_{i}].to_frame()
     y = df[energy_cons]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
