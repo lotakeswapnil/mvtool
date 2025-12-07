a0ebf6cdae3113ca29462f429912774cdacd3907
lotakeswapnil <lotakeswapnil@yahoo.com>
2025-12-07 19:28:52 +0530

    regression

--- mvtool.py
+++ mvtool.py
@@ -162,7 +162,7 @@ elif st.session_state.mode == "manual":
 
         lat = st.number_input("Latitude", format="%.4f")
         lon = st.number_input("Longitude", format="%.4f")
-        start_date = st.date_input("Start date", value=date.today().replace(year=date.today().year-1.day-1))
+        start_date = st.date_input("Start date", value=date.today().replace(year=date.today().year-1).replace(day=date.today().day-1))
         end_date = st.date_input("End date", value=date.today())
         var = "temperature_2m"   # or let user pick
         which = "hourly"
