b8261cdc248f8fb6d14dd9b99c10c0f149173a65
lotakeswapnil <lotakeswapnil@yahoo.com>
2025-12-07 13:47:02 +0530

    regression

--- mvtool.py
+++ mvtool.py
@@ -25,6 +25,8 @@ if st.session_state.mode is None:
             st.session_state.mode = "upload"
 
 
+st.write(st.session_state.mode)
+
 # --- Only show uploader & modeling inputs after button is clicked ---
 if st.session_state.mode == "upload":
     uploaded = st.file_uploader('Upload CSV', type='csv')
