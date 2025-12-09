f86c57a7e5d24408064972ddc5304cb203aed902
lotakeswapnil <lotakeswapnil@yahoo.com>
2025-12-09 11:38:49 +0530

    regression

--- mvtool.py
+++ mvtool.py
@@ -228,7 +228,7 @@ elif st.session_state.mode == "manual":
                         st.write(df_weather_final)
 
 
-        st.write(pd.concat([manual_df, df_weather_final]).reset_index())
+        st.write(pd.concat([manual_df, df_weather_final]).axis=1)
 
 
 
