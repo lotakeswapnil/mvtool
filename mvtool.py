076a4dcdb3865cb90027269102c3200544475fae
lotakeswapnil <lotakeswapnil@yahoo.com>
2025-12-09 11:27:01 +0530

    regression

--- mvtool.py
+++ mvtool.py
@@ -218,20 +218,23 @@ elif st.session_state.mode == "manual":
                         st.write(df_weather_final)
 
                         st.session_state.df_weather_final = df_weather_final
-            # Build column names automatically
-            col_names = ["Dependent Variable"]  # first column fixed
-            empty_df = pd.DataFrame("", index=range(0), columns=col_names)
 
-            st.write('#### Enter Dependent Variable Below:')
+                        if st.session_state.df_weather_final is not None:
+                            # Build column names automatically
+                            col_names = ["Dependent Variable"]  # first column fixed
+                            empty_df = pd.DataFrame("", index=range(0), columns=col_names)
 
-            manual_df = st.data_editor(empty_df, num_rows="dynamic")
+                            st.write('#### Enter Dependent Variable Below:')
 
-            if st.button('Create Data'):
-                st.success('Generated Data:')
-                st.dataframe(manual_df)
+                            manual_df = st.data_editor(empty_df, num_rows="dynamic")
 
-                manual_df = pd.concat([df_monthly], ignore_index=True)
-                st.write(manual_df)
+                            if st.button('Create Data'):
+                                st.success('Generated Data:')
+                                st.dataframe(manual_df)
+
+                                manual_df = pd.concat([df_monthly], ignore_index=True)
+                                st.write(manual_df)
+
 
 
 
