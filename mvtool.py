01115ddec1c092af9a2874e0e6713d7159589175
lotakeswapnil <lotakeswapnil@yahoo.com>
2025-12-09 14:39:34 +0530

    regression

--- mvtool.py
+++ mvtool.py
@@ -303,7 +303,9 @@ elif st.session_state.mode == "manual":
 
             st.write('#### Enter Energy Data Below:')
 
-            final_df = st.dataframe(st.data_editor(empty_df, num_rows="dynamic"))
+            edited_df = st.data_editor(empty_df, num_rows="dynamic")
+
+            final_df = st.dataframe(edited_df)
 
             # Sidebar settings
             st.sidebar.header("Model settings")
