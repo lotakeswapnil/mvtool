29438d54799ab800afba1116ad0fa5ee2f8d2ca2
lotakeswapnil <lotakeswapnil@yahoo.com>
2025-12-09 14:41:53 +0530

    regression

--- mvtool.py
+++ mvtool.py
@@ -305,7 +305,7 @@ elif st.session_state.mode == "manual":
 
             edited_df = st.data_editor(empty_df, num_rows="dynamic")
 
-            final_df = st.dataframe(edited_df)
+            final_df = st.dataframe(edited_df).dropna()
 
             # Sidebar settings
             st.sidebar.header("Model settings")
