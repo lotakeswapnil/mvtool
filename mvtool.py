1b34cde1c015f5eba6654f446cf12a8a010dea68
lotakeswapnil <lotakeswapnil@yahoo.com>
2025-12-12 12:50:24 +0530

    regression

--- mvtool.py
+++ mvtool.py
@@ -668,8 +668,8 @@ elif st.session_state.mode == "manual":
                 start_date = manual_df['Start Date (yyyy-mm-dd)'][i].date().isoformat()
                 end_date = manual_df['End Date (yyyy-mm-dd)'][i].date().isoformat()
                 meta, temperature_data = fetch_openmeteo_archive(client, lat, lon, start_date, end_date, which,var)
-                average_temp = temperature_data["temperature"].mean()
-                st.write(average_temp)
+                manual_df['temperature'] = temperature_data["temperature"].mean()
+            st.write(manual_df)
 
         if st.session_state.interval == 'no':
 
