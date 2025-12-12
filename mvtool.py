d15d7e059a454ec4cb92f0dc03925cecc07f4b57
lotakeswapnil <lotakeswapnil@yahoo.com>
2025-12-12 12:08:07 +0530

    regression

--- mvtool.py
+++ mvtool.py
@@ -652,7 +652,7 @@ elif st.session_state.mode == "manual":
 
             # Build column names automatically
             col_names = ['Start Date','End Date','Energy']  # first column fixed
-            empty_df = pd.DataFrame({'Start Date': pd.Series([dt.date.today()], dtype='object'),'End Date': pd.Series([0], dtype='datetime64[ns]'),
+            empty_df = pd.DataFrame({'Start Date': pd.Series([0], dtype='datetime64[ns]'),'End Date': pd.Series([0], dtype='datetime64[ns]'),
                                      'Energy': pd.Series([0], dtype=float)})
 
             st.write('#### Enter Energy Data Below:')
