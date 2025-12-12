ae1d5ae423218924fce6967a063434ea2014c2dd
lotakeswapnil <lotakeswapnil@yahoo.com>
2025-12-12 12:38:02 +0530

    regression

--- mvtool.py
+++ mvtool.py
@@ -665,7 +665,7 @@ elif st.session_state.mode == "manual":
 
 
             for i in range(len(manual_df)):
-                st.write(manual_df['Start Date'][i])
+                st.write(manual_df['Start Date (yyyy-mm-dd)'][i])
 
 
         if st.session_state.interval == 'no':
