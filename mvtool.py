c04aff97b5a44e7102f1018add2dcdfe8f2eef2a
lotakeswapnil <lotakeswapnil@yahoo.com>
2025-12-20 12:32:12 +0530

    regression

--- mvtool.py
+++ mvtool.py
@@ -3,6 +3,8 @@ from datetime import date
 
 import pandas as pd
 import streamlit as st
+from numpy.conftest import dtype
+
 from supplementary.change_point import (fit_three_param_cp, fit_five_param_deadband, select_model_by_rmse_r2,
                                         predict_3p_for_plot, predict_5p_for_plot)
 from sklearn.linear_model import LinearRegression, Lasso, Ridge
@@ -272,7 +274,7 @@ elif st.session_state.mode == "upload":
                     # PLOT MODELS
                     # -------------------------
 
-                    st.write(type(temp_data['Temperature']))
+                    st.write(dtype(temp_data['Temperature']))
                     T_plot = np.linspace(temp_data['Temperature'].min(), temp_data['Temperature'].max(), 400)
 
                     fig, ax = plt.subplots(figsize=(9, 5))
