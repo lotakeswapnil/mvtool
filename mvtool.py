6ab408f014d0959647d2d03ba14be67a5322908b
lotakeswapnil <lotakeswapnil@yahoo.com>
2025-12-20 20:58:28 +0530

    regression

--- mvtool.py
+++ mvtool.py
@@ -117,8 +117,9 @@ elif st.session_state.mode == "upload":
 
         with rep2:
             if rep_ind_var == 'Independent Variable':
+                st.write(f'##### Reported Data Independent Variable Names:')
                 for i in range(1, base_num_var + 1):
-                    st.write(f'##### Reported Data Independent Variable Names: \n {globals()[f"ind_var_{i}"]})
+                    st.write(globals()[f"ind_var_{i}"])
 
             else:
                 energy_data = st.text_input('Energy column name')
