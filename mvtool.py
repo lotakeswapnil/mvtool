01d86ea5bb2c4c658acfac4913e8cd20c93c0fd9
lotakeswapnil <lotakeswapnil@yahoo.com>
2025-12-20 11:59:31 +0530

    regression

--- mvtool.py
+++ mvtool.py
@@ -122,19 +122,7 @@ elif st.session_state.mode == "upload":
 
                     st.write(f'R2: {regression:.2%}')
                     st.write(f'CV (RMSE): {cvrmse:.2%}')
-
-                    chart_df = pd.DataFrame({
-                        independent[0]: X.iloc[:, 0],
-                        "Actual": y,
-                        "Predicted": preds
-                    })
-
-                    st.line_chart(
-                        chart_df,
-                        x=independent[0],
-                        y=["Actual", "Predicted"]
-                    )
-
+                    st.line_chart(pd.DataFrame({'Actual': y, 'Predicted': preds}).reset_index(drop=True))
 
 
 
