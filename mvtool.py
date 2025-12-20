bd203659c76c936bed93c5a096019fd2ae8f87aa
lotakeswapnil <lotakeswapnil@yahoo.com>
2025-12-20 12:30:35 +0530

    regression

--- mvtool.py
+++ mvtool.py
@@ -271,6 +271,8 @@ elif st.session_state.mode == "upload":
                     # -------------------------
                     # PLOT MODELS
                     # -------------------------
+
+                    print(type(temp_data))
                     T_plot = np.linspace(temp_data['Temperature'].min(), temp_data['Temperature'].max(), 400)
 
                     fig, ax = plt.subplots(figsize=(9, 5))
