a07885170cb68358b4579ca8bef79d2b892b2c09
lotakeswapnil <lotakeswapnil@yahoo.com>
2025-12-31 10:17:26 +0530

    regression

--- mvtool.py
+++ mvtool.py
@@ -271,7 +271,7 @@ elif st.session_state.mode == "upload":
                     if model_choice == "3-parameter" or model_choice == "5-parameter":
                         st.write(f'##### Predicted Baseline Consumption: \n {pred_r.sum():.2f}')
 
-                    else:
+                    #else:
                         mod1, mod2 = st.columns(2)
                         with mod1:
                             st.write(f'##### 3 Parameter Predicted Baseline Consumption: \n {pred_r_3p.sum():.2f}')
