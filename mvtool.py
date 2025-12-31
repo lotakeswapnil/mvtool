02f22d84006b3c9c5a426c682806c18377a88133
lotakeswapnil <lotakeswapnil@yahoo.com>
2025-12-31 11:01:19 +0530

    regression

--- mvtool.py
+++ mvtool.py
@@ -389,7 +389,6 @@ elif st.session_state.mode == "upload":
 
                     # --------------------------
 
-
                     y_r = df_r[rep_energy]
                     x_r = df_r[temp_data]
                     if model_choice == "3-parameter":
