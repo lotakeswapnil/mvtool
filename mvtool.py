154e1b68db4d8a92fd2b2e617b1060925d194fa5
lotakeswapnil <lotakeswapnil@yahoo.com>
2025-12-31 11:27:12 +0530

    regression

--- mvtool.py
+++ mvtool.py
@@ -156,7 +156,6 @@ elif st.session_state.mode == "upload":
                         cvrmse = root_mean_squared_error(y, preds)/y.mean()
 
                         #--------------------------------------------------
-
                         y_r = df_r[rep_energy]
                         x_r = df_r[independent]
                         pred_r = model.predict(x_r)
