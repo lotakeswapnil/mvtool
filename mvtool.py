d69e1899014b3eba9e8ef4c84e0fb64363b72481
lotakeswapnil <lotakeswapnil@yahoo.com>
2026-01-01 18:56:04 +0530

    regression

--- mvtool.py
+++ mvtool.py
@@ -13,7 +13,7 @@ if "mode" not in st.session_state:
 
 st.subheader("Select Any One of the Options")
 
-mode = st.radio(options=["Manual Entry", "Upload Data"], index=None, disabled=False)
+mode = st.radio('',options=["Manual Entry", "Upload Data"], index=None, disabled=False)
 
 # -------------------------
 # UPLOAD DATA MODE
