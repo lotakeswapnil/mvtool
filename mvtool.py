e86c920f9150d8177b63ab256e340ec8cbc29428
lotakeswapnil <lotakeswapnil@yahoo.com>
2026-01-03 10:14:51 +0530

    regression

--- mvtool.py
+++ mvtool.py
@@ -4,22 +4,25 @@ import streamlit as st
 st.title('Energy M&V Tool')
 
 
-# --- Display Start Buttons ---
+# --- Select Mode of Analysis ---
 
-mode = st.radio('Select Any One of the Options',options=["Manual Data", "Upload Data (CSV or Excel)"], index=None)
+mode = st.radio('',options=["Manual Data", "Upload Data (CSV or Excel)"], index=None)
+
+
+with st.spinner('Loading...'):
 
 # -------------------------
 # UPLOAD DATA MODE
 # -------------------------
 
-if mode == "Upload Data (CSV or Excel)":
-    from supplementary.upload import upload_page
-    upload_page()
+    if mode == "Upload Data (CSV or Excel)":
+        from supplementary.upload import upload_page
+        upload_page()
 
 # -------------------------
 # MANUAL DATA MODE
 # -------------------------
 
-elif mode == "Manual Data":
-    from supplementary.manual import manual_page
-    manual_page()
+    elif mode == "Manual Data":
+        from supplementary.manual import manual_page
+        manual_page()
