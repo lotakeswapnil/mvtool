336e1ba9a490f5f14bd29855fb6d844adbdba671
lotakeswapnil <lotakeswapnil@yahoo.com>
2026-01-15 10:22:28 +0530

    regression

--- supplementary/model_results.py
+++ supplementary/model_results.py
@@ -3,7 +3,7 @@ import streamlit as st
 def three_para_results(three_res, temperature_unit, mean_energy):
 
     st.subheader("3-Parameter Model")
-    st.write("#### Model Equation")
+    st.write("#### Equation")
 
     Tb = three_res["Tb"]
     mode_used = three_res["mode"]
@@ -29,7 +29,7 @@ def three_para_results(three_res, temperature_unit, mean_energy):
     # ----------------------------
     # Model results
     # ----------------------------
-    st.write("#### Model Results")
+    st.write("#### Results")
 
     if temperature_unit == "celsius":
         st.write(f"**Balance Temperature (Tb):** {Tb:.2f} °C")
@@ -49,13 +49,13 @@ def three_para_results(three_res, temperature_unit, mean_energy):
 
 def five_para_results(five_res,temperature_unit,mean_energy):
     st.subheader("5-Parameter Model")
-    st.write("#### Model Equations")
+    st.write("#### Equation")
     st.latex(
         fr"\text{{Energy}} = {five_res['model'].intercept_:.2f} + "
         fr"{five_res['model'].coef_[0]:.2f}\,\max(0,\,{five_res['Tb_low']:.2f} - T) + "
         fr"{five_res['model'].coef_[1]:.2f}\,\max(0,\,T - {five_res['Tb_high']:.2f})"
     )
-    st.write("#### Model Results")
+    st.write("#### Results")
     if temperature_unit == 'celsius':
         st.write(f"**Tb_low:** {five_res['Tb_low']:.2f} °C")
     else:
@@ -74,7 +74,7 @@ def three_five_para_results(three_res,five_res,temperature_unit,mean_energy):
     col1, col2 = st.columns(2, border=True)
     with col1:
         st.subheader("3-Parameter Model")
-        st.write("#### Model Equations")
+        st.write("#### Equation")
         Tb = three_res["Tb"]
         b0 = three_res["model"].intercept_
         b1 = three_res["model"].coef_[0]
@@ -91,7 +91,7 @@ def three_five_para_results(three_res,five_res,temperature_unit,mean_energy):
             st.latex(
                 fr"\text{{Energy}} = {b0:.2f} + {b1:.2f}\,\max(0,\,{Tb:.2f} - T)"
             )
-        st.write("#### Model Results")
+        st.write("#### Results")
         if temperature_unit == 'celsius':
             st.write(f"**Tb:** {three_res['Tb']:.2f} °C")
         else:
@@ -102,13 +102,13 @@ def three_five_para_results(three_res,five_res,temperature_unit,mean_energy):
         st.write(f"**R²:** {three_res['r2']:.2%}")
     with col2:
         st.subheader("5-Parameter Model")
-        st.write("#### Model Equations")
+        st.write("#### Equation")
         st.latex(
             fr"\text{{Energy}} = {five_res['model'].intercept_:.2f} + "
             fr"{five_res['model'].coef_[0]:.2f}\,\max(0,\,{five_res['Tb_low']:.2f} - T) + "
             fr"{five_res['model'].coef_[1]:.2f}\,\max(0,\,T - {five_res['Tb_high']:.2f})"
         )
-        st.write("#### Model Results")
+        st.write("#### Results")
         if temperature_unit == 'celsius':
             st.write(f"**Tb_low:** {five_res['Tb_low']:.2f} °C")
         else:
